83cdd0b56e50edc521d3d6d69463525f4516d474
Patrick Njiru <patricknjiru93@gmail.com>
2024-02-07 04:41:50 +0300

    Put items in list.

--- src/App.jsx
+++ src/App.jsx
@@ -32,208 +32,210 @@ const App = () => {
 	return (
 		<div className="w-75" style={styles.font}>
 			<div className="container">
-				<nav className='fw-bold m-3'>
+				<nav className='fw-bold m-5 mb-0'>
 					Navbar 
 					<span className="rounded-circle bg-dark text-light px-2 p-1 ms-2">
 						{ itemsCount }
 					</span>
 				</nav>
 			
-				<div className="mt-3 ms-5">
+				<div className="ms-5">
 					<button 
-						className="btn btn-primary mt-4"
+						className="btn btn-primary my-5"
 						onClick={() => { setItemsCount(0), setCount1(0), setCount2(0), setCount3(0), setCount4(0) }}
 					>
 						Reset
 					</button>
 
-					{/* Item 1 */}
-					<div 
-						className={displayItem1 + "align-items-start justify-content-between my-3"} 
-						style={styles.width}
-					>
-						{count1 == 0 ? 
-							<div className="alert bg-warning fw-bold py-0 px-2"> Zero </div> 
-							: 
-							<div className="alert bg-primary fw-bold py-0 px-2"> {count1} </div> 
-						}
+					<ul className='list-group mt-3'>
+						{/* Item 1 */}
+						<li 
+							className={displayItem1 + "list-group-item align-items-start justify-content-between border-0"} 
+							style={styles.width}
+						>
+							{count1 == 0 ? 
+								<div className="alert bg-warning fw-bold py-0 px-2"> Zero </div> 
+								: 
+								<div className="alert bg-primary fw-bold py-0 px-2"> {count1} </div> 
+							}
 
-						<div className="w-75">
-							<button 
-								type='button' 
-								className="btn btn-dark py-2 mx-2 fw-bold"
-								onClick={() => {
-									setCount1(count1 => count1 += 1)
-									handleCountChange(count1, '+')
-								}} 
-							>
-								+
-							</button>
+							<div className="w-75">
+								<button 
+									type='button' 
+									className="btn btn-dark py-2 mx-2 fw-bold"
+									onClick={() => {
+										setCount1(count1 => count1 += 1)
+										handleCountChange(count1, '+')
+									}} 
+								>
+									+
+								</button>
 
-							<button
-								type='button'
-								className={`btn btn-dark py-2 mx-2 fw-bold + ${count1 == 0 ? ' disabled' : null}`}
-								onClick={() => {
-									count1 > 0 ? setCount1(count1 => count1 -= 1) : null
-									handleCountChange(count1, '-')
-								}} 
-							>
-								&minus;
-							</button>
+								<button
+									type='button'
+									className={`btn btn-dark py-2 mx-2 fw-bold + ${count1 == 0 ? ' disabled' : null}`}
+									onClick={() => {
+										count1 > 0 ? setCount1(count1 => count1 -= 1) : null
+										handleCountChange(count1, '-')
+									}} 
+								>
+									&minus;
+								</button>
 
-							<button
-								type='button' 
-								className="btn btn-danger py-2 mx-2 fw-bold"
-								onClick={() => {
-									setDisplayItem1('d-none ')
-									count1 > 0 ? handleCountChange(1, '-') : null
-								}}
-							>
-								Delete
-							</button>
-						</div>
-					</div>
+								<button
+									type='button' 
+									className="btn btn-danger py-2 mx-2 fw-bold"
+									onClick={() => {
+										setDisplayItem1('d-none ')
+										count1 > 0 ? handleCountChange(1, '-') : null
+									}}
+								>
+									Delete
+								</button>
+							</div>
+						</li>
 
-					{/* Item 2 */}
-					<div 
-						className={displayItem2 + "align-items-start justify-content-between my-3"} 
-						style={styles.width}
-					>
-						{count2 == 0 ? 
-							<div className="alert bg-warning fw-bold py-0 px-2"> Zero </div> 
-							: 
-							<div className="alert bg-primary fw-bold py-0 px-2"> {count2} </div> 
-						}
+						{/* Item 2 */}
+						<li 
+							className={displayItem2 + "list-group-item align-items-start justify-content-between border-0"} 
+							style={styles.width}
+						>
+							{count2 == 0 ? 
+								<div className="alert bg-warning fw-bold py-0 px-2"> Zero </div> 
+								: 
+								<div className="alert bg-primary fw-bold py-0 px-2"> {count2} </div> 
+							}
 
-						<div className="w-75">
-							<button 
-								type='button' 
-								className="btn btn-dark py-2 mx-2 fw-bold"
-								onClick={() => {
-									setCount2(count2 => count2 += 1)
-									handleCountChange(count2, '+')
-								}} 
-							>
-								+
-							</button>
+							<div className="w-75">
+								<button 
+									type='button' 
+									className="btn btn-dark py-2 mx-2 fw-bold"
+									onClick={() => {
+										setCount2(count2 => count2 += 1)
+										handleCountChange(count2, '+')
+									}} 
+								>
+									+
+								</button>
 
-							<button
-								type='button'
-								className={`btn btn-dark py-2 mx-2 fw-bold + ${count2 == 0 ? ' disabled' : null}`}
-								onClick={() => {
-									count2 > 0 ? setCount2(count2 => count2 -= 1) : null
-									handleCountChange(count2, '-')
-								}} 
-							>
-								&minus;
-							</button>
+								<button
+									type='button'
+									className={`btn btn-dark py-2 mx-2 fw-bold + ${count2 == 0 ? ' disabled' : null}`}
+									onClick={() => {
+										count2 > 0 ? setCount2(count2 => count2 -= 1) : null
+										handleCountChange(count2, '-')
+									}} 
+								>
+									&minus;
+								</button>
 
-							<button 
-								type='button'
-								className="btn btn-danger py-2 mx-2 fw-bold"
-								onClick={() => {
-									setDisplayItem2('d-none ')
-									count2 > 0 ? handleCountChange(1, '-') : null
-								}}
-							>
-								Delete
-							</button>
-						</div>
-					</div>
+								<button 
+									type='button'
+									className="btn btn-danger py-2 mx-2 fw-bold"
+									onClick={() => {
+										setDisplayItem2('d-none ')
+										count2 > 0 ? handleCountChange(1, '-') : null
+									}}
+								>
+									Delete
+								</button>
+							</div>
+						</li>
 
-					{/* Item 3 */}
-					<div 
-						className={displayItem3 + "align-items-start justify-content-between my-3"} 
-						style={styles.width}
-					>
-						{count3 == 0 ? 
-							<div className="alert bg-warning fw-bold py-0 px-2"> Zero </div> 
-							: 
-							<div className="alert bg-primary fw-bold py-0 px-2"> {count3} </div> 
-						}
+						{/* Item 3 */}
+						<li 
+							className={displayItem3 + "list-group-item align-items-start justify-content-between border-0"} 
+							style={styles.width}
+						>
+							{count3 == 0 ? 
+								<div className="alert bg-warning fw-bold py-0 px-2"> Zero </div> 
+								: 
+								<div className="alert bg-primary fw-bold py-0 px-2"> {count3} </div> 
+							}
 
-						<div className="w-75">
-							<button 
-								type='button' 
-								className="btn btn-dark py-2 mx-2 fw-bold"
-								onClick={() => {
-									setCount3(count3 => count3 += 1)
-									handleCountChange(count3, '+')
-								}} 
-							>
-								+
-							</button>
+							<div className="w-75">
+								<button 
+									type='button' 
+									className="btn btn-dark py-2 mx-2 fw-bold"
+									onClick={() => {
+										setCount3(count3 => count3 += 1)
+										handleCountChange(count3, '+')
+									}} 
+								>
+									+
+								</button>
 
-							<button
-								type='button'
-								className={`btn btn-dark py-2 mx-2 fw-bold + ${count3 == 0 ? ' disabled' : null}`}
-								onClick={() => {
-									count3 > 0 ? setCount3(count3 => count3 -= 1) : null
-									handleCountChange(count3, '-')
-								}} 
-							>
-								&minus;
-							</button>
+								<button
+									type='button'
+									className={`btn btn-dark py-2 mx-2 fw-bold + ${count3 == 0 ? ' disabled' : null}`}
+									onClick={() => {
+										count3 > 0 ? setCount3(count3 => count3 -= 1) : null
+										handleCountChange(count3, '-')
+									}} 
+								>
+									&minus;
+								</button>
 
-							<button
-								type='button' 
-								className="btn btn-danger py-2 mx-2 fw-bold"
-								onClick={() => {
-									setDisplayItem3('d-none ')
-									count3 > 0 ? handleCountChange(1, '-') : null
-								}}
-							>
-								Delete
-							</button>
-						</div>
-					</div>
+								<button
+									type='button' 
+									className="btn btn-danger py-2 mx-2 fw-bold"
+									onClick={() => {
+										setDisplayItem3('d-none ')
+										count3 > 0 ? handleCountChange(1, '-') : null
+									}}
+								>
+									Delete
+								</button>
+							</div>
+						</li>
 
-					{/* Item 4 */}
-					<div 
-						className={displayItem4 + "align-items-start justify-content-between my-3"} 
-						style={styles.width}
-					>
-						{count4 == 0 ? 
-							<div className="alert bg-warning fw-bold py-0 px-2"> Zero </div> 
-							: 
-							<div className="alert bg-primary fw-bold py-0 px-2"> {count4} </div> 
-						}
+						{/* Item 4 */}
+						<li 
+							className={displayItem4 + "list-group-item align-items-start justify-content-between border-0"} 
+							style={styles.width}
+						>
+							{count4 == 0 ? 
+								<div className="alert bg-warning fw-bold py-0 px-2"> Zero </div> 
+								: 
+								<div className="alert bg-primary fw-bold py-0 px-2"> {count4} </div> 
+							}
 
-						<div className="w-75">
-							<button 
-								type='button' 
-								className="btn btn-dark py-2 mx-2 fw-bold"
-								onClick={() => {
-									setCount4(count4 => count4 += 1)
-									handleCountChange(count4, '+')
-								}} 
-							>
-								+
-							</button>
+							<div className="w-75">
+								<button 
+									type='button' 
+									className="btn btn-dark py-2 mx-2 fw-bold"
+									onClick={() => {
+										setCount4(count4 => count4 += 1)
+										handleCountChange(count4, '+')
+									}} 
+								>
+									+
+								</button>
 
-							<button
-								type='button'
-								className={`btn btn-dark py-2 mx-2 fw-bold + ${count4 == 0 ? ' disabled' : null}`}
-								onClick={() => {
-									count4 > 0 ? setCount4(count4 => count4 -= 1) : null
-									handleCountChange(count4, '-')
-								}} 
-							>
-								&minus;
-							</button>
+								<button
+									type='button'
+									className={`btn btn-dark py-2 mx-2 fw-bold + ${count4 == 0 ? ' disabled' : null}`}
+									onClick={() => {
+										count4 > 0 ? setCount4(count4 => count4 -= 1) : null
+										handleCountChange(count4, '-')
+									}} 
+								>
+									&minus;
+								</button>
 
-							<button 
-								type='button'
-								className="btn btn-danger py-2 mx-2 fw-bold"
-								onClick={() => {
-									setDisplayItem4('d-none ')
-									count1 > 0 ? handleCountChange(1, '-') : null
-								}}
-							>
-								Delete
-							</button>
-						</div>
-					</div>
+								<button 
+									type='button'
+									className="btn btn-danger py-2 mx-2 fw-bold"
+									onClick={() => {
+										setDisplayItem4('d-none ')
+										count1 > 0 ? handleCountChange(1, '-') : null
+									}}
+								>
+									Delete
+								</button>
+							</div>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</div>
